282772e01ca13509a5d054630f783f1898fcee36
Matteo <mrodrigues7@myges.fr>
2024-03-05 01:15:22 +0100

    add security

--- api/signup.go
+++ api/signup.go
@@ -1,49 +1,55 @@
 package api
 
 import (
-	"auth/db"
-	"auth/models"
-	"context"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"os"
+    "auth/db"
+    "auth/models"
+    "context"
+    "encoding/json"
+    "io/ioutil"
+    "net/http"
+    "os"
 
-	"golang.org/x/crypto/bcrypt"
+    "golang.org/x/crypto/bcrypt"
+    "go.mongodb.org/mongo-driver/bson"
 )
 
 func SignupHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO : SUPPRIMER POUR PROD
-	// Set CORS headers
-	w.Header().Set("Access-Control-Allow-Origin", "*") // or specify your domain
-	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-	
-	jwtSecret := os.Getenv("JWT_SECRET")
-    if jwtSecret == "" {
-		http.Error(w, "JWT secret not found", http.StatusInternalServerError)
-    }
-	
-    db.Connect()
-	
-	input := models.User{}
-	
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Erreur lors de la lecture du corps de la requête", http.StatusInternalServerError)
-		return
-	}
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.Header().Set("Access-Control-Allow-Methods", "POST")
+    w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-	// Décodage du JSON dans la structure IInput
-	err = json.Unmarshal(body, &input)
-	if err != nil {
-		http.Error(w, "Erreur lors de la conversion du JSON", http.StatusInternalServerError)
-		return
-	}
+    jwtSecret := os.Getenv("JWT_SECRET")
+    if jwtSecret == "" {
+        http.Error(w, "JWT secret not found", http.StatusInternalServerError)
+        return
+    }
+
+    db.Connect()
+
+    var input models.User
+
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        http.Error(w, "Erreur lors de la lecture du corps de la requête", http.StatusInternalServerError)
+        return
+    }
+
+    err = json.Unmarshal(body, &input)
+    if err != nil {
+        http.Error(w, "Erreur lors de la conversion du JSON", http.StatusInternalServerError)
+        return
+    }
+
+    var existingUser models.User
+    err = db.Collection.FindOne(context.TODO(), bson.M{"email": input.Email}).Decode(&existingUser)
+    if err == nil {
+        http.Error(w, "Email is already in use", http.StatusBadRequest)
+        return
+    }
 
     hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
     if err != nil {
-		http.Error(w, "Password hashing failed", http.StatusInternalServerError)
+        http.Error(w, "Password hashing failed", http.StatusInternalServerError)
         return
     }
 
@@ -51,8 +57,9 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 
     _, err = db.Collection.InsertOne(context.TODO(), input)
     if err != nil {
-		http.Error(w, "User creation failed", http.StatusInternalServerError)
+        http.Error(w, "User creation failed", http.StatusInternalServerError)
         return
     }
-	w.Write([]byte("User created successfully"))
+
+    w.Write([]byte("User created successfully"))
 }
